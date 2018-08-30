474c0c7f165261a3eb9d5c0194f6649286f39031
duffikins <42541535+duffikins@users.noreply.github.com>
2018-08-30 17:05:53 +0530

    Update master.sh

--- setup/master.sh
+++ setup/master.sh
@@ -14,12 +14,25 @@ ftppasswd=`< /dev/urandom tr -dc A-Za-z0-9 | head -c20; echo`
 rpcuser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c15; echo`
 rpcpassword=`< /dev/urandom tr -dc A-Za-z0-9 | head -c20; echo`
 
+
+echo -e \
+'--------------------------------------------'"\n"\
+'Parameter'"\n"\
+'--------------------------------------------'"\n"\
+'chainname='$1"\n"\
+'rpcport='$3"\n"\
+'networkport='$2"\n\n"\
+
+
+
+
+
 bash -e hardening.sh
 bash -e lamp.sh $db_root_pass
 bash -e phpmyadmin.sh
 bash -e ftp.sh $ftpusername $ftppasswd
 bash -e core.sh $rpcuser $rpcpassword $db_admin_user $db_admin_pass
-bash -e configure-yobichain.sh $db_root_pass $db_admin_user $db_admin_pass
+bash -e configure-yobichain.sh $db_root_pass $db_admin_user $db_admin_pass $1 $2 $3
 
 echo -e \
 '--------------------------------------------'"\n"\
